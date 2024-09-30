acbb608db4ffd0fceb15f7de9ccbd8e6d3cc0747
ram-meegada <ramumeegada1811@gmail.com>
2024-10-01 00:23:52 +0530

    added create table functionality

--- db_dashboard_app/views.py
+++ db_dashboard_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
-
+import psycopg2
 
 class DashBoardView(TemplateView):
     template_name = "dashboard/dashboard.html"
@@ -9,7 +9,22 @@ class DashBoardView(TemplateView):
 
 class CreateTableView(TemplateView):
     template_name = "createTable/create_table.html"
-    def get(self, request):
-        
+    def post(self, request):
+        print(request.POST, '========requsjdb--------')
+        conn = psycopg2.connect(
+            database="database_dashboard_db", 
+            user="postgres",
+            password="Ramu@123",
+            host="localhost",
+            port=5432
+            )
+        cursor = conn.cursor()
+        cursor.execute('''create table public."User1" (
+                            id serial primary key,
+                            name varchar(50)
+                        )''')
+        conn.commit()
+        cursor.close()
+        conn.close()
         return render(request, self.template_name)
 
